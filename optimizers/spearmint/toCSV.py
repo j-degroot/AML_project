e6919de13e43958c091eecedb24a8fad6ec20b36
saleha1wer <95382833+saleha1wer@users.noreply.github.com>
2021-12-06 00:27:21 +0100

    Add files via upload

--- optimizers/spearmint/toCSV.py
+++ optimizers/spearmint/toCSV.py
@@ -7,18 +7,26 @@ header = ['lrate', 'l2_reg','n_epochs','loss']
 data = []
 
 #for each of the 100 evaluations
-for i in range(1,6):
-
-    with open('/Users/mrsalwer/Downloads/Spearmint-master/examples/simple/output/0000000'+ str(i) + '.out') as f:
-        lines = f.readlines()
+for i in range(1,50): 
+    if len(str(i)) == 1:
+        with open('/Users/mrsalwer/Downloads/Spearmint-master/examples/simple/output/0000000'+ str(i) + '.out') as f:
+            lines = f.readlines()
+    elif len(str(i)) == 2:
+        with open('/Users/mrsalwer/Downloads/Spearmint-master/examples/simple/output/000000'+ str(i) + '.out') as f:
+            lines = f.readlines()
+    elif len(str(i)) == 3:
+        with open('/Users/mrsalwer/Downloads/Spearmint-master/examples/simple/output/00000'+ str(i) + '.out') as f:
+            lines = f.readlines()
 
 
     result = lines[-1:][0] #loss
     var3 = lines[-2:][0] #n_epochs
     var2 = lines[-3:][0] #l2_reg
+    print(var2)
     var1 = lines[-4:][0] #lrate
 
-    data.append([var1[:-2],var2[:-2],var3[-2],result[-2]])
+
+    data.append([var1,var2,var3,result])
 
 
 #create csv file
@@ -27,6 +35,5 @@ with open('/Users/mrsalwer/Downloads/Spearmint-master/examples/simple/run.csv', 
     writer = csv.writer(c)
     writer.writerow(header)
     for row in data:
-        print(row)
         writer.writerow(row)
 

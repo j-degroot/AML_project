c44a1c249f8e4468686eaa3ec01671ff42f0838c
j-degroot <jurrengroot@hotmail.nl>
2021-12-12 12:57:47 +0100

    renamed json-to-csv.py to json_to_csv.py

--- optimizers/smac/json_to_csv.py
+++ optimizers/smac/json_to_csv.py
@@ -1,26 +1,20 @@
-# convert JSON output data to csv
-
 import json
+import csv
 
-'/Users/Jurren/Documents/GitHub/AML_project/smac/smac3-output_data'
-import os
-os.getcwd()
+for i in range(10):
+    lines = []
+    f = open('SMAC-DATA-RAW/run_'+str(i)+'/runhistory.json')
+    data = json.load(f)
+    
 
+    for z in range(1,101):
+        line = data['configs'][str(z)]
+        loss = data['data'][z-1][1][0]
+        lines.append([line['lrate'], line['l2_reg'], line['n_epochs'], loss])
+        
 
-for i in range(1,11):
-    # Opening JSON file
-    json_file = f'/optimizers/sma'
-
-    f = open('data.json')
-
-# returns JSON object as
-# a dictionary
-data = json.load(f)
-
-# Iterating through the json
-# list
-for i in data['emp_details']:
-    print(i)
-
-# Closing file
-f.close()
+    with open('SMAC-csv/' + 'smac' + str(i) +'.csv', 'w') as c:
+        writer = csv.writer(c)
+        writer.writerow(['lrate', 'l2_reg','n_epochs','loss'])
+        for line in lines:
+            writer.writerow(line)
